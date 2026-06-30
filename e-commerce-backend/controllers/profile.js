d6c5e8efcd60a878f44051812103b641f53cfb9c
atifazad560 <atifazad560@gmail.com>
2026-06-30 23:41:36 +0530

    created updateProfile controller & routing of getProfile-updateProfile in router/user.js

--- e-commerce-backend/controllers/profile.js
+++ e-commerce-backend/controllers/profile.js
@@ -8,7 +8,7 @@ exports.getProfile = async (req, res) => {
 
         const userId = req.user._id;
 
-        const user =  await User.findById(userId).populate(`userType`);
+        const user = await User.findById(userId).populate(`userType`);
 
         if (!user) {
             return res.status(400).json({
@@ -38,5 +38,61 @@ exports.getProfile = async (req, res) => {
             success: false,
             message: "Failed to fetch profile"
         });
+    }
+}
+
+
+
+
+exports.updateProfile = async (req, res) => {
+
+    try {
+        const { name, mobile, address, dob, gender } = req.body;
+        const userId = req.user._id
+        const user = await User.findById(userId)
+
+        if (!user) {
+            return res.status(404).json({
+                success: false,
+                message: "User not found"
+            })
+        };
+
+        if (name) {
+            user.name = name
+        }
+
+
+        if (mobile) {
+            user.mobile = mobile
+        }
+
+
+        if (address) {
+            user.address = address
+        }
+
+
+        if (dob) {
+            user.dob = dob
+        }
+
+
+        if (gender) {
+            user.gender = gender
+        }
+
+        await user.save();
+
+        res.status(200).json({
+            success: true,
+            message: "Profile updated successfully !!"
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: `Internal server error`
+        })
     }
 }
--- e-commerce-backend/routes/user.js
+++ e-commerce-backend/routes/user.js
@@ -5,22 +5,27 @@ const router = express.Router();
 const { login, signup, verifyEmail, forgotPassword, resetPassword, getAllUsers, getAllProducts, getProducts, addToWishList, getWishList, addtoCart, getCart, removeCart } = require('../controllers/user');
 const { validate } = require('../middleware/validation');
 const { authUser } = require('../middleware/auth');
+const { getProfile, updateProfile } = require('../controllers/profile');
 
 // User Authentication Routes
 router.post("/signup", signup);
 router.post("/login", login);
-router.post("/verify-email",  verifyEmail);
+router.post("/verify-email", verifyEmail);
 // Password Reset Routes
 router.post("/forgot-password", forgotPassword);
-router.post("/reset-password",  resetPassword);
+router.post("/reset-password", resetPassword);
 
-router.get("/get-Allusers",  getAllUsers);
-router.get("/get-all-products" , getAllProducts);
-router.get("/products/:id" ,authUser, getProducts);
+router.get("/get-Allusers", getAllUsers);
+router.get("/get-all-products", getAllProducts);
+router.get("/products/:id", authUser, getProducts);
 
-router.post("/add-wishlist" ,authUser, addToWishList);
-router.get("/get-wishlist" ,authUser, getWishList);
+router.post("/add-wishlist", authUser, addToWishList);
+router.get("/get-wishlist", authUser, getWishList);
 
+
+
+router.get(`/get-profile`, authUser, getProfile);
+router.put(`/update-profile`, authUser, updateProfile);
 // router.post('/add-cart', authUser, addtoCart);
 // router.get('/get-cart', authUser, getCart);
 // router.delete('/remove-cart/:id', authUser, removeCart);
